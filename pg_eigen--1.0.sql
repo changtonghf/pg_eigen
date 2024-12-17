972ff6fb6e22140f28ebbb4ed0229eec69eab052
chang tong <2210360857@qq.com>
2024-12-17 05:20:09 -0500

    format SQL

--- pg_eigen--1.0.sql
+++ pg_eigen--1.0.sql
@@ -1,25 +1,16 @@
+CREATE FUNCTION array_reduce(text,   int2[], int[]) RETURNS   int2[] AS 'MODULE_PATHNAME' LANGUAGE C IMMUTABLE;
+CREATE FUNCTION array_reduce(text,   int4[], int[]) RETURNS   int4[] AS 'MODULE_PATHNAME' LANGUAGE C IMMUTABLE;
+CREATE FUNCTION array_reduce(text,   int8[], int[]) RETURNS   int8[] AS 'MODULE_PATHNAME' LANGUAGE C IMMUTABLE;
 CREATE FUNCTION array_reduce(text, float4[], int[]) RETURNS float4[] AS 'MODULE_PATHNAME' LANGUAGE C IMMUTABLE;
-
 CREATE FUNCTION array_reduce(text, float8[], int[]) RETURNS float8[] AS 'MODULE_PATHNAME' LANGUAGE C IMMUTABLE;
 
-CREATE FUNCTION array_reduce(text, int2[], int[]) RETURNS int2[] AS 'MODULE_PATHNAME' LANGUAGE C IMMUTABLE;
-
-CREATE FUNCTION array_reduce(text, int4[], int[]) RETURNS int4[] AS 'MODULE_PATHNAME' LANGUAGE C IMMUTABLE;
-
-CREATE FUNCTION array_reduce(text, int8[], int[]) RETURNS int8[] AS 'MODULE_PATHNAME' LANGUAGE C IMMUTABLE;
-
 CREATE FUNCTION array_fft(boolean, float4[], int[]) RETURNS float4[] AS 'MODULE_PATHNAME' LANGUAGE C IMMUTABLE;
-
 CREATE FUNCTION array_fft(boolean, float8[], int[]) RETURNS float8[] AS 'MODULE_PATHNAME' LANGUAGE C IMMUTABLE;
 
 CREATE FUNCTION array_random(text, int4[], float8, float8) RETURNS float8[] AS 'MODULE_PATHNAME' LANGUAGE C;
 
-CREATE FUNCTION array_shuffle(int2[], int) RETURNS int2[] AS 'MODULE_PATHNAME' LANGUAGE C;
-
-CREATE FUNCTION array_shuffle(int4[], int) RETURNS int4[] AS 'MODULE_PATHNAME' LANGUAGE C;
-
-CREATE FUNCTION array_shuffle(int8[], int) RETURNS int8[] AS 'MODULE_PATHNAME' LANGUAGE C;
-
+CREATE FUNCTION array_shuffle(  int2[], int) RETURNS   int2[] AS 'MODULE_PATHNAME' LANGUAGE C;
+CREATE FUNCTION array_shuffle(  int4[], int) RETURNS   int4[] AS 'MODULE_PATHNAME' LANGUAGE C;
+CREATE FUNCTION array_shuffle(  int8[], int) RETURNS   int8[] AS 'MODULE_PATHNAME' LANGUAGE C;
 CREATE FUNCTION array_shuffle(float4[], int) RETURNS float4[] AS 'MODULE_PATHNAME' LANGUAGE C;
-
 CREATE FUNCTION array_shuffle(float8[], int) RETURNS float8[] AS 'MODULE_PATHNAME' LANGUAGE C;
